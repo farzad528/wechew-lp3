3c5ba3e7d13ed3b4446880f56342f03280a238f6
Farzad Sunavala <fsunavala1@gmail.com>
2021-01-06 10:39:25 -0600

    testimonials

--- src/views/LandingPage/Sections/TeamSection.js
+++ src/views/LandingPage/Sections/TeamSection.js
@@ -16,7 +16,7 @@ import CardFooter from "components/Card/CardFooter.js";
 
 import styles from "assets/jss/material-kit-react/views/landingPageSections/teamStyle.js";
 
-import team1 from "assets/img/faces/farzad.jpg";
+import team1 from "assets/img/faces/blank.jpg";
 import team2 from "assets/img/faces/natalia.jpg";
 import team3 from "assets/img/faces/kendall.jpg";
 
@@ -34,90 +34,59 @@ export default function TeamSection() {
       <h2 className={classes.title}>What our chewers are saying</h2>
       <div>
         <GridContainer>
-          <GridItem xs={12} sm={12} md={6}>
+          <GridItem xs={12} sm={12} md={4}>
             <Card plain>
               <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
                 <img src={team1} alt="..." className={imageClasses} />
               </GridItem>
               <h4 className={classes.cardTitle}>
-                Farzad Sunavala
-                <br />
-                <small className={classes.smallTitle}>Co-Founder</small>
+                Bahrom F
+                {/* <br />
+                <small className={classes.smallTitle}>Co-Founder</small> */}
               </h4>
               <CardBody>
                 <p className={classes.description}>
-                  Farzad’s roles and responsibilities include technical delivery
-                  of the product throughout all life cycles of development
-                  operations. Farzad is also responsible for owning the product
-                  backlog with assistance of his UX Research and Design team.
+                  <i>"Great and fun way to swipe through restaraunts with my coworkers to pick where we eat for lunch"</i>
                 </p>
               </CardBody>
-              <CardFooter className={classes.justifyCenter}>
-                <Button
-                  justIcon
-                  color="transparent"
-                  className={classes.margin5}
-                >
-                  <i className={classes.socials + " fab fa-twitter"} />
-                </Button>
-                <Button
-                  justIcon
-                  color="transparent"
-                  className={classes.margin5}
-                >
-                  <i className={classes.socials + " fab fa-instagram"} />
-                </Button>
-                <Button
-                  justIcon
-                  color="transparent"
-                  className={classes.margin5}
-                >
-                  <i className={classes.socials + " fab fa-linkedin"} />
-                </Button>
-              </CardFooter>
             </Card>
           </GridItem>
-          <GridItem xs={12} sm={12} md={6}>
+          <GridItem xs={12} sm={12} md={4}>
             <Card plain>
               <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
-                <img src={team2} alt="..." className={imageClasses} />
+                <img src={team1} alt="..." className={imageClasses} />
               </GridItem>
               <h4 className={classes.cardTitle}>
-                Natalia Vornic
-                <br />
-                <small className={classes.smallTitle}>Co-Founder</small>
+                Fram H
+                {/* <br />
+                <small className={classes.smallTitle}>Co-Founder</small> */}
               </h4>
               <CardBody>
                 <p className={classes.description}>
-                  Natalia’s roles and responsibilities include non-technical
-                  solution delivery through service lifecycle. Natalia is
-                  responsible for user engagement and user research,
-                  communication, branding and promotions.
+                  <i>"We often spend more time deciding where to eat than actually eating once we’ve decided. WeChew helps us save so much time!"</i>
                 </p>
               </CardBody>
-              <CardFooter className={classes.justifyCenter}>
-                <Button
-                  justIcon
-                  color="transparent"
-                  className={classes.margin5}
-                >
-                  <i className={classes.socials + " fab fa-twitter"} />
-                </Button>
-                <Button
-                  justIcon
-                  color="transparent"
-                  className={classes.margin5}
-                >
-                  <i className={classes.socials + " fab fa-instagram"} />
-                </Button>
-                <Button
-                  justIcon
-                  color="transparent"
-                  className={classes.margin5}
-                >
-                  <i className={classes.socials + " fab fa-linkedin"} />
-                </Button>
-              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <Card plain>
+              <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
+                <img src={team1} alt="..." className={imageClasses} />
+              </GridItem>
+              <h4 className={classes.cardTitle}>
+                Dana F
+                {/* <br />
+                <small className={classes.smallTitle}>Co-Founder</small> */}
+              </h4>
+              <CardBody>
+                <p className={classes.description}>
+                  <i>
+                    "WeChew helps me be more inclusive and make sure that
+                    everyone has a pleasant dining experience independent of
+                    dietary restrictions. This is a win-win for all!"
+                  </i>
+                </p>
+              </CardBody>
             </Card>
           </GridItem>
         </GridContainer>
